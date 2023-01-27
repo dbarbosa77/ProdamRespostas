5814c494eb040b07bfbf7b95b2049fbb60e2dddb
Douglas Rodrigues Barbosa <99232015+dbarbosa77@users.noreply.github.com>
2023-01-27 01:23:17 -0300

    Exercicio2 - Resposta

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿// 2. Escreva um algoritmo (pseudocódigo ou fluxograma) para achar o menor valor da seguinte lista de inteiros: 
+// var list = new List<int>() { 1, 3, 2, 8, 9, 15, 41,12, 1, 17, 28 }; 
+
+
+int MenorValor = 1000000;
+
+var list = new List<int>() { 1, 3, 2, 8, 9, 15, 41,12, 1, 17, 28 }; 
+
+foreach(var item in list){
+    if(item < MenorValor)
+    MenorValor = item;
+}
+
+Console.WriteLine($"O menor valor da lista é {MenorValor}");
+
+
+// PSEUDOCÓDIGO
+
+// inteiro MenorValor = 1000000;
+
+
+// para (cada item na lista){
+//     se (o Item atual da lista for menor que a variavel MenorValor)
+//          MenorValor = Item
+//}
+// MenorValor irá ser o menor item da lista. 
+
